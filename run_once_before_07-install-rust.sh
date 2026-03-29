a8989609e0b6afe165f9eecbe0595a891b61aa7a
Hari Srinivasan <harisrini21@gmail.com>
2026-03-29 10:48:00 +0000

    windows path error fixup

--- run_once_before_07-install-rust.sh
+++ run_once_before_07-install-rust.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-if ! command -v rustc >/dev/null; then
+if [ ! -x "$HOME/.cargo/bin/rustc" ]; then
   echo "🦀 Installing Rust toolchain..."
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path
   source "$HOME/.cargo/env"
